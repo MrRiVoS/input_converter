babebef2caad501b9f627cd1331461778530922b
Ivens Lima <ivens_cml@outlook.com>
2026-06-28 00:44:08 -0300

    Add parsing of keywords GRID, DI, DJ and DK

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,49 @@ use std::fs;
 use std::path::PathBuf;
 
 #[derive(Parser)]
-struc Args {
+struct Args {
   input: PathBuf,
+}
+
+#[derive(Debug)]
+enum Keyword {
+    Grid,
+    Di,
+    Dj,
+    Dk,
+    Unknown(String),
+}
+
+fn classify_keyword(line :&str) -> Option<Keyword> {
+    let word = line.trim().split_whitespace().next()?;
+
+    // remove the '*' if it exists
+    let word = word.strip_prefix('*').unwrap_or(word);
+
+    match word.to_uppercase().as_str() {
+        "GRID" => Some(Keyword::Grid),
+        "DI"   => Some(Keyword::Di),
+        "DJ"   => Some(Keyword::Dj),
+        "DK"   => Some(Keyword::Dk),
+        other  => Some(Keyword::Unknown(other.to_string())),
+    }
 }
 
 fn main() {
 
-  let args = Args::parse();
+    let args = Args::parse();
 
-  let input = fs::read_to_string(&args.input)
-    .expect("Could not read input file.");
+    let input_bytes = fs::read(&args.input)
+      .expect("Could not read input file.");
 
-  println!("{input}");
+    let input_text: String = input_bytes.into_iter().map(|b| b as char).collect();
+
+    // println!("File size: {} bytes", input.len());
+    // println!("{input_text}");
+    for (i, line) in input_text.lines().enumerate() {
+        if let Some(keyword) = classify_keyword(line) {
+            println!("{:04}: {:?} -> {}", i + 1, keyword, line);
+        }
+    }
+
 }
